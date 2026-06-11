d35f6c3ba71e2a41d11dff43f820009c633e67c2
mohinimraut7 <mohinimraut7@gmail.com>
2026-06-11 17:29:10 +0530

    backend register api done

---
+++ app/Http/Middleware/Kernel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middleware = [
+        \Illuminate\Http\Middleware\HandleCors::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \Illuminate\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ];
+
+    protected $middlewareGroups = [
+        'api' => [
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+
+    protected $middlewareAliases = [
+        'auth.jwt' => \App\Http\Middleware\JwtAuthMiddleware::class,
+    ];
+}
